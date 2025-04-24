46d7df23a2407141ce0ce6e4179edf1915896af0
Duda Monteiro <monteiroflaviaeduarda868@gmail.com>
2025-04-24 18:55:08 -0300

    atualiza repositorio

--- app.js
+++ app.js
@@ -1,28 +1,93 @@
-import express from 'express'
-import usuariosRoutes from './routes/usuarios.js'
-import fornecedoresRoutes from './routes/fornecedores.js'
-import produtosRoutes from './routes/produtosFake.js'
+import express from "express"
+
+import produtosRouter from "./router/produtosFake.js"
 
 const app = express()
 
-// Permitir ler JSON no corpo da requisição
+// Permite ler JSON no corpo da requisição  
 app.use(express.json())
-
-// Adiciona o roteador de usuários
-app.use("/usuarios", usuariosRoutes)
-
-// Adiciona o roteador de fornecedores
-app.use("/fornecedores", fornecedoresRoutes)
-
-// Adiciona o roteador de produtos
-app.use("/produtos", produtosRoutes)
 
 const port = 3000
 
-app.get("/", (req, res)=>{
-  res.send("Oi")
+
+let usuarios = [
+    {
+        id: 1,
+        nome: "duda",
+        email: "duda@gmail.com"
+    },{
+        id: 2,
+        nome: "flavia",
+        email: "flavia@gmail.com"
+    }
+]
+
+app.use("/produtos", produtosRouter)
+
+app.put("/usuario/:id", (req, res) => {
+    const { id } = req.params
+    const { nome, email } = req.body
+
+    // const usuario = usuarios.find(
+    //     usuario => usuario.id === parseInt(id)
+    // )
+    // usuario.nome = novoNome
+    // usuario.email = novoEmail
+    
+    const indice = usuarios.findIndex((usuario) => {
+        return usuario.id == id
+    })
+    
+    usuarios[indice].nome = nome
+    usuarios[indice].email = email
+
+    res.send(usuarios)
+} )
+
+
+app.delete("/usuario/:id", (req, res) => {
+
+    const { id } = req.params
+    
+    const indice = usuarios.findIndex((usuario) => {
+        return usuario.id == id
+    })
+     
+    
+
+usuarios.splice(indice, 1)
+res.send(usuarios)
+
+})
+//Banco de dados da shoop (em memoria)
+
+
+
+app.get("/usuarios", (req, res) => {
+    res.send(usuarios)
 })
 
-app.listen(port, ()=>{
-    console.log(`App escutando na  porta ${port}`)
+// app.get("/", (req, res) => {
+//     res.send("Bem vindo a minha API! ")
+// })
+
+app.post("/criarUsuario", (req, res) => {
+    const { nome, email }  = req.body
+
+    const id = usuarios[usuarios.length - 1].id + 1
+
+    const pessoaNova = {
+        "id": id,
+        nome,
+        email
+    }
+
+    usuarios.push(pessoaNova)
+    
+    res.send(usuarios)
+
+})  
+
+app.listen(port, () => {
+    console.log(`Exemple app listening on port ${port}`)
 })
